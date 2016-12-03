5df408493d40bcd633b74ca817838feb1633c2a8
mustafa <mh17114@gmail.com>
2016-12-03 10:16:28 +0000

    First commit: adding all the files to the repository

--- app/assets/javascripts/quote.js
+++ app/assets/javascripts/quote.js
@@ -11,25 +11,31 @@ function validate(){
         return false;
     }
     if(ph.value == ""){
-       
+        alert( "Please provide your phone no!");
         return false;
     } 
     if(em.value == ""){
         
-        
+         alert( "Please provide your valid email!");
         return false;
     } 
     if(add.value == ""){
-        
+         alert( "Please provide your full address!");
         return false;
     }
 
      if(com.value == ""){
         com.style.backgroundColor="red";
-        alert( "Please provide your full address!");
+        alert( "Please provide short description about service which you are looking for!!!");
         return false;
      }
 }
+
+function resetForm() 
+{ 
+document.reset(); 
+} 
+
 //Function To Display Popup
 function div_show() {
 document.getElementById('abc').style.display = "block";
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :quote_details
   root 'welcome#index'
   get 'quote' => 'welcome#quote' 
-
+  get 'index' => 'welcome#index'
+  get 'wel' => 'welcome#wel'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
